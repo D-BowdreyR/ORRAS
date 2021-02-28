2a21335c0f7ffe23f2df24d2fadc4bfda4b01eb5
DanBR <47244055+D-BowdreyR@users.noreply.github.com>
2021-02-28 15:35:58 +0000

    Wire up db migration and seeding data on start

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -3,17 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ORRA.Infrastructure.Persistence;
 
 namespace WebUI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host  = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+
+                    if (context.Database.IsSqlite())
+                    {
+                        await context.Database.MigrateAsync();
+                    }
+                    // run the seed data method
+                    await Seed.SeedDataAsync(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred while migrating or seeding the database");
+
+                    throw;
+                }
+            }
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
