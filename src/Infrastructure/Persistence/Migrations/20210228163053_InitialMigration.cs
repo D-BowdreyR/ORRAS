d00aca5cd1a7f8f03a73600b2bdf8e7db1c5898b
DanBR <47244055+D-BowdreyR@users.noreply.github.com>
2021-02-28 16:33:46 +0000

    Initial ef Migration to generate sql to create db

---
+++ src/Infrastructure/Persistence/Migrations/20210228163053_InitialMigration.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ORRA.Infrastructure.Persistence.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Modalities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ModalityCode = table.Column<string>(type: "TEXT", nullable: true),
+                    ModalityName = table.Column<string>(type: "TEXT", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modalities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ImagingProcedures",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CrisCode = table.Column<string>(type: "TEXT", nullable: true),
+                    Term = table.Column<string>(type: "TEXT", nullable: true),
+                    ExamCount = table.Column<int>(type: "INTEGER", nullable: false),
+                    ModalityId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Interventional = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImagingProcedures", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ImagingProcedures_Modalities_ModalityId",
+                        column: x => x.ModalityId,
+                        principalTable: "Modalities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImagingProcedures_ModalityId",
+                table: "ImagingProcedures",
+                column: "ModalityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modalities_ModalityCode",
+                table: "Modalities",
+                column: "ModalityCode");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ImagingProcedures");
+
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.DropTable(
+                name: "Modalities");
+        }
+    }
+}
