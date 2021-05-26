1e32978d472b7a16c0430bbfb328eafcbbe1cfab
DanBR <47244055+D-BowdreyR@users.noreply.github.com>
2021-05-26 12:09:17 +0100

    Added API agent with Axios http client for managing client-side http requests

---
+++ src/WebUI/client-app/src/app/api/agent.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosResponse } from 'axios';
+
+const sleep = (delay: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay)
+    })
+}
+axios.defaults.baseURL = "http://localhost:5000/api";
+
+axios.interceptors.response.use(async response => {
+    try {
+        await sleep(1000);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return await Promise.reject(error);
+    }
+})
+
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+const requests = {
+    get: <T> (url: string) => axios.get<T>(url).then(responseBody),
+    post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+}
+
+// http methods for a resource go here
+
+
+
+const agent = {
+
+}
+
+export default agent;
